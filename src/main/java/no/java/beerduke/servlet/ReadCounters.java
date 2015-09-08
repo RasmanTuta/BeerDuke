d0ef67a8bb981d1b756b679b401ae1ca37e0848a
Kristian Berg <rasmantuta@gmail.com>
2015-09-08 16:23:23 +0200

    Added servlet for reading counters

---
+++ src/main/java/no/java/beerduke/servlet/ReadCounters.java
@@ -0,0 +1,30 @@
+package no.java.beerduke.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by kristian on 08.09.2015.
+ */
+public class ReadCounters extends DukeServlet {
+    public void init() throws ServletException
+    {
+        // Do required initialization
+        initController();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final int[] counters = this.counters.readCounters();
+        response.setContentType("text/plain");
+
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println(countersString(counters));
+
+    }
+}
